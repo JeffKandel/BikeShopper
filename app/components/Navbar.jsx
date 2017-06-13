090fbe29569727b18287e113e1ba87e81a901436
Student <jeff.kandel@gmail.com>
2017-06-13 19:38:29 -0400

    refactor: use tachyons for Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Link, browserHistory } from 'react-router'
 import Login from './Login'
 import WhoAmI from './WhoAmI'
+import NavbarLink from './NavbarLink'
 
 /* -----------------    COMPONENT     ------------------ */
 
@@ -13,28 +14,27 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-inverse">
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to="/" >Lisa's Bikes</Link>
-          </li>
-          <li>
-            <Link to="/products" >Bicycles</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li>
-            <Link to="/users">Admin Only: All Users</Link>
-          </li>
-          <li>
-            <Link to="/authenticate" >Sign Up</Link>
-          </li>
-          <li>
-            {this.props.user ? <WhoAmI/> : <Login/>}
-          </li>
-        </ul>
-      </nav>
+      <header className="w-100 pa3 ph5-ns bg-white">
+        <div className="db dt-ns mw9 center w-100">
+        <div className="db dtc-ns v-mid tl w-50">
+          <Link to="/" className="dib f5 f4-ns fw6 mt0 mb1 link black-70">
+            Bike Shopper&nbsp;
+          </Link>
+          <div className="dib">
+            <small className="nowrap f6 mt2 mt3-ns pr2 black-70 fw2">
+               styled with tachyons.io
+            </small>
+          </div>
+        </div>
+        <nav className="db dtc-ns v-mid w-100 tl tr-ns mt2 mt0-ns">
+            <NavbarLink to="/products" title="Bicycles"/>
+            <NavbarLink to="/cart" title="Cart"/>
+            <NavbarLink to="/users" title="Admin Only: All Users"/>
+            <NavbarLink to="/authenticate" title="Sign Up"/>
+            {true ? <WhoAmI/> : <Login/>}
+        </nav>
+        </div>
+      </header>
     )
   }
 }
---
+++ app/components/NavbarLink.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default (props) => (
+  <Link className="f6 fw6 hover-blue link black-70 mr2 mr3-m mr4-l dib" to={props.to} title={props.title}>{props.title}</Link>
+)
