ee1f50d3ecd45a15d7af650de774520fac6b41c9
Student <jeff.kandel@gmail.com>
2017-06-17 23:42:30 -0400

    refactor: add flexbox layout

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Header from './Navigation/Header'
 
 const Root = ({ children }) => (
-  <div className='flex cf'>
-    <Header className=""/>
-    <main className="bg-blue mt5 pt4 pt3-m pt1-l">
+  <div className='flex flex-column'>
+    <Header />
+    <main className="w-100 bg-blue">
       { children }
     </main>
   </div>
