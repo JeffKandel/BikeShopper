6a68fd359c19eaa5ae559d9cb3a4dd338e01a5b6
Student <jeff.kandel@gmail.com>
2017-06-18 02:07:42 -0400

    refactor: test possible story tile

--- app/components/Home/ImageLink.jsx
+++ app/components/Home/ImageLink.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default () => (
-    <Link to='/products' className="flex  aspect-ratio aspect-ratio--1x1 dim  h-100 justify-center items-center">
-      <span
-        role="img"
-        aria-label="Beyoncé"
-        style={{backgroundImage: `url(http://via.placeholder.com/700)`}}
-        className="bg-center cover aspect-ratio--object">
+
+    <a href="#" style={{backgroundImage: `url(http://via.placeholder.com/700)`}}
+
+      className="flex link aspect-ratio aspect-ratio--1x1 hide-child cover bg-center">
+      <span className="white dtc v-mid aspect-ratio--object child bg-black-40">
+        Card title
       </span>
-    </Link>
+    </a>
 )
+
+// <Link to='/products' className="flex  aspect-ratio aspect-ratio--1x1 dim  h-100 justify-center items-center">
+//       <span
+//         role="img"
+//         aria-label="Beyoncé"
+//         style={{backgroundImage: `url(http://via.placeholder.com/700)`}}
+//         className="bg-center cover aspect-ratio--object">
+//       </span>
+//     </Link>
