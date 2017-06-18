b304def75eb435d59a5390bf4e6a543bab21fa65
Student <jeff.kandel@gmail.com>
2017-06-18 14:52:25 -0400

    refactor: add text to CategoryLink

--- app/components/Home/CategoryLink.jsx
+++ app/components/Home/CategoryLink.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default ({category}) => (
-  <Link to='/products' className="db aspect-ratio aspect-ratio--1x1 dim h-100">
+  <Link to='/products' className="flex align-center aspect-ratio aspect-ratio--1x1 dim h-100">
     <span
       role="img"
       aria-label={category}
       className="bg-center bg-black cover aspect-ratio--object">
+        Text
     </span>
   </Link>
 )
