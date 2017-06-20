f6dac0c3c06738ff000092376a66dbc8fc2a2421
Student <jeff.kandel@gmail.com>
2017-06-19 22:57:14 -0400

    refactor: clean dead code out of SingleProduct

--- app/components/Products/SingleProduct.jsx
+++ app/components/Products/SingleProduct.jsx
@@ -12,33 +12,10 @@ class SingleProduct extends React.Component {
 
   constructor(props) {
     super(props)
-
-    this.state = {
-      color: '',
-      size: '',
-      quantity: 0
-    }
-    this.setColor = this.setColor.bind(this)
-    this.setSize = this.setSize.bind(this)
-    this.setQuantity = this.setQuantity.bind(this)
-  }
-
-  setColor(val) {
-    this.setState({ color: val })
-  }
-
-  setSize(val) {
-    this.setState({ size: val })
-  }
-
-  setQuantity(val) {
-    this.setState({ quantity: val })
   }
 
   render() {
     const product = this.props.singleProduct
-    console.log("product", product);
-    const reviews = this.props.reviews
 
     return (
       <article className="flex cf ph3 ph5-ns pv5">
@@ -53,12 +30,14 @@ class SingleProduct extends React.Component {
             <price className="f6 ttu tracked overflow-scroll gray">
               {formatPrice(product.price)}
             </price>
-            <a className="fr no-underline near-white bg-animate bg-mid-gray hover-bg-black inline-flex items-center ma2 tc br2 pa2" href="https://facebook.com" title="Facebook">
+            <div
+              className="fr no-underline near-white bg-animate bg-mid-gray hover-bg-black inline-flex items-center ma2 tc br2 pa2"
+            >
               <FontAwesome name='shopping-cart'/>
               <span className="f6 ml2 pr2">
                 Add to Cart
               </span>
-            </a>
+            </div>
           </div>
         </header>
         <div className="fl w-two-thirds-ns pa2">
@@ -79,18 +58,3 @@ class SingleProduct extends React.Component {
 export default connect(
   state => ({ singleProduct: state.product.selectedProduct }), {}
 )(SingleProduct)
-
-// <div>
-//   <div className = "col-md-6">
-//     <div className="carousel carousel-slider">
-//       <a className="carousel-item" href="#one!">
-//         {
-//           product.images && product.images.map(image => (
-//             <img src={ image }  />
-//           ))
-//         }
-//       </a>
-//     </div>
-//   </div>
-
-
