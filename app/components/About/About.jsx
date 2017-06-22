5ea4f1e963a1740f56a0d128c3a844eb95f0fd0a
Student <jeff.kandel@gmail.com>
2017-06-22 15:16:52 -0400

    fix: edit about text

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 export default () => (
-     <article className="pa3 pa5-ns">
-       <h1 className="">Title</h1>
-       <p className="measure lh-copy">
-         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-         tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-         vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-         no sea takimata sanctus est Lorem ipsum dolor sit amet.
-       </p>
-       <p className="measure lh-copy">
-         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-         tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-         vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-         no sea takimata sanctus est Lorem ipsum dolor sit amet.
-       </p>
+     <article className="cf ph3 ph5-ns pv5">
+       <header className="fn fl-ns w-50-ns pr4-ns">
+         <h1 className="mb3 mt0 lh-title">About Page</h1>
+         <time className="f6 ttu tracked gray">Jeff Kandel</time>
+       </header>
+       <div className="fn fl-ns w-50-ns">
+         <p className="lh-copy measure mt4 mt0-ns">
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat ut ligula sit amet tempor. Suspendisse sit amet varius mauris. Vivamus ullamcorper lacus non commodo pharetra. Integer sapien ex, ultricies in facilisis sit amet, pretium nec sapien. Etiam magna urna, egestas eget nisl id, auctor consequat elit. In quis neque molestie, facilisis mi et, efficitur nisl. Nulla pulvinar ultrices diam et mollis. Duis nec faucibus metus
+         </p>
+         <p className="lh-copy measure">
+           Vivamus dictum tincidunt quam, eu mattis arcu lacinia eget. Cras semper placerat suscipit. Vestibulum elementum sapien vel odio maximus, sed egestas ex vestibulum. Duis hendrerit nisl sed velit suscipit, nec cursus tortor auctor. Aenean elementum quam quis nisi condimentum, a ultricies felis cursus. Suspendisse potenti. Nulla vel nisi vestibulum, scelerisque elit sed, blandit quam. Donec erat tortor, facilisis eu accumsan in, lobortis eu dolor. Duis a tristique nisi. Quisque tempus nisi ut enim fringilla, sodales suscipit magna fermentum. Integer suscipit blandit ornare. Pellentesque eget est sagittis, porta ex nec, aliquet nunc. Quisque in dictum metus.
+         </p>
+         <p className="lh-copy measure">
+           Praesent quis ex turpis. Etiam non purus nec tellus viverra volutpat eu et diam. Phasellus nisl justo, consectetur ac suscipit vel, interdum congue eros. Vivamus dictum orci a sapien lobortis, vel fermentum mi interdum. Sed tincidunt augue ut fermentum malesuada. Donec interdum ut nulla mollis pulvinar. Donec finibus augue non leo dictum iaculis. Nunc sed arcu velit. Quisque fringilla a eros et posuere.
+         </p>
+       </div>
      </article>
 )
