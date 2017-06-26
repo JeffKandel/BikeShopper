1f22834be851e59d54fb6e5c46dbcf6c99a8f67f
Student <jeff.kandel@gmail.com>
2017-06-26 11:54:22 -0400

    fix: improve text on About Page

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router'
+import FontAwesome from 'react-fontawesome'
 
 export default () => (
-     <article className="cf ph3 ph5-ns pv5">
-       <header className="fn fl-ns w-50-ns pr4-ns">
-         <h1 className="mb3 mt0 lh-title">About Page</h1>
-         <time className="f6 ttu tracked gray">Jeff Kandel</time>
+  <article className="cf ph3 ph5-ns pv5">
+       <header className="fn fl-ns tr w-50-ns pr4-ns">
+         <h1 className="mb3 mt0 lh-title">Designing Bike Shopper</h1>
+         <time>
+           <a className="f6 ttu tracked gray link no-underline" href='http://www.github.com/JeffKandel'>
+              <FontAwesome name='github'/>Jeff Kandel
+           </a>
+         </time>
        </header>
        <div className="fn fl-ns w-50-ns">
          <p className="lh-copy measure mt4 mt0-ns">
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat ut ligula sit amet tempor. Suspendisse sit amet varius mauris. Vivamus ullamcorper lacus non commodo pharetra. Integer sapien ex, ultricies in facilisis sit amet, pretium nec sapien. Etiam magna urna, egestas eget nisl id, auctor consequat elit. In quis neque molestie, facilisis mi et, efficitur nisl. Nulla pulvinar ultrices diam et mollis. Duis nec faucibus metus
+          Bike Shopper was not always this pretty.  When it was first built, part of a school project utilizing React for the first time, style was secondary.  The emphasis at the time was on component architecture and building features specified by the assignment.  Much of it worked and some of it looked passable, but nothing stood out and I was not happy with the product.  After a month or so away from the code, I decided to come back and redesign the whole site.
          </p>
          <p className="lh-copy measure">
-           Vivamus dictum tincidunt quam, eu mattis arcu lacinia eget. Cras semper placerat suscipit. Vestibulum elementum sapien vel odio maximus, sed egestas ex vestibulum. Duis hendrerit nisl sed velit suscipit, nec cursus tortor auctor. Aenean elementum quam quis nisi condimentum, a ultricies felis cursus. Suspendisse potenti. Nulla vel nisi vestibulum, scelerisque elit sed, blandit quam. Donec erat tortor, facilisis eu accumsan in, lobortis eu dolor. Duis a tristique nisi. Quisque tempus nisi ut enim fringilla, sodales suscipit magna fermentum. Integer suscipit blandit ornare. Pellentesque eget est sagittis, porta ex nec, aliquet nunc. Quisque in dictum metus.
+            Paragraph about Tachyons and React:
+              - mature inline styles
+                - Applying atomic classes was a great refresher of CSS fundamentals
+              - React components were easy to edit and maintain
          </p>
          <p className="lh-copy measure">
-           Praesent quis ex turpis. Etiam non purus nec tellus viverra volutpat eu et diam. Phasellus nisl justo, consectetur ac suscipit vel, interdum congue eros. Vivamus dictum orci a sapien lobortis, vel fermentum mi interdum. Sed tincidunt augue ut fermentum malesuada. Donec interdum ut nulla mollis pulvinar. Donec finibus augue non leo dictum iaculis. Nunc sed arcu velit. Quisque fringilla a eros et posuere.
+           Links and things:
+            - code at github
+            - Jeff github
+            - Jeff Tech talk
+
          </p>
        </div>
      </article>
--- app/components/Navigation/NavbarLink.jsx
+++ app/components/Navigation/NavbarLink.jsx
@@ -4,7 +4,7 @@ import FontAwesome from 'react-fontawesome'
 
 export default ({ to, title, logo, onClick }) => (
   <Link
-    className="f6 fw6 hide-s hover-blue hide-child link black-70 mr2 mr3-m mr4-l dib"
+    className="f6 fw6 hover-blue link black-70 mr2 mr3-m mr4-l dib"
     to={to}
     title={title}
     onClick={onClick}
