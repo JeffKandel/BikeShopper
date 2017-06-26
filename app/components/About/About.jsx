a5706d619d117f5c48a425f18570e16ff10e133e
Student <jeff.kandel@gmail.com>
2017-06-26 15:51:30 -0400

    fix: improve text on About Page

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -3,36 +3,28 @@ import { Link } from 'react-router'
 import FontAwesome from 'react-fontawesome'
 
 export default () => (
-  <article className="cf ph3 ph5-ns pv5">
-       <header className="fn fl-ns tr w-50-ns pr4-ns">
-         <h1 className="mb3 mt0 lh-title">Designing Bike Shopper</h1>
-         <time>
-           <a className="f6 ttu tracked gray link no-underline" href='http://www.github.com/JeffKandel'>
-              <FontAwesome name='github'/>Jeff Kandel
-           </a>
-         </time>
-       </header>
-       <div className="fn fl-ns w-50-ns">
-         <p className="lh-copy measure mt4 mt0-ns">
-          Bike Shopper was not always this pretty.  When it was first built, part of a school project utilizing React for the first time, style was secondary.  The emphasis at the time was on component architecture and building features specified by the assignment.  Much of it worked and some of it looked passable, but nothing stood out and I was not happy with the product.  After a month or so away from the code, I decided to come back and redesign the whole site.
-         </p>
-         <p className="lh-copy measure">
-            Paragraph about Tachyons and React:
-              - mature inline styles
-                - Applying atomic classes was a solid refresher of CSS fundamentals
-              - styling with tachyons for mobile first ended up leading to a very clear and minimal web design overall.
-              - embeding atomic CSS into React made it super easy to maintain style
-              - When did scaling disappear as a concern?  I didn't even notice.
-            Would I do it again:
-              - Yes, I'll be working on my personal site next and plan to use Tachyons.  If I were working on a larger project, I might also build a style guide to help other developers, but I would do this for any group project I was maintaining.
-         </p>
-         <p className="lh-copy measure">
-           Links and things:
-            - code at github
-            - Jeff github
-            - Jeff Tech talk
-
-         </p>
-       </div>
-     </article>
+<article className="cf ph3 ph5-ns pv5">
+  <header className="fn fl-ns tr w-50-ns pr4-ns">
+    <h1 className="mb3 mt0 lh-title">Designing Bike Shopper</h1>
+    <time>
+      <a className="f6 ttu tracked gray link no-underline" target="_blank" href='http://www.github.com/JeffKandel'>
+      <FontAwesome name='github'/> Jeff Kandel //
+      </a><br/>
+      <a className="f6 ttu tracked gray link no-underline" target="_blank" href='http://www.github.com/JeffKandel/BikeShopper'>
+      <FontAwesome name='github'/> Bike Shopper //
+      </a><br/>
+      <a className="f6 ttu tracked gray link no-underline" target="_blank" href='https://www.youtube.com/watch?v=SVLUh0rn6_4'>
+      <FontAwesome name='youtube'/> Functional CSS //
+      </a><br/>
+    </time>
+  </header>
+  <div className="fn fl-ns w-50-ns">
+    <p className="lh-copy measure mt4 mt0-ns">
+    Bike Shopper is not a store and it was never intended to be one.  It is a design experiment with some meat on its bones.  I had been reading a lot about functional CSS and given <a className=" gray link no-underline" target="_blank" href='https://www.youtube.com/watch?v=SVLUh0rn6_4'>a brief talk</a> on the subject.  After experimenting with this design approach, I was looking for a larger sandbox to explore functional CSS alongside React.  I decided to strip the CSS from an old school project and started from scratch with Tachyons CSS library.  The site you see is a result of that experimentation.
+    </p>
+    <p className="lh-copy measure">
+    After working with Tachyons and React, I feel really positive about the combination.  Functional CSS feels close to the styling metal and the practice of writing out each class reinforced CSS fundamentals along the way.  Embedding these styles in React components ensured that maintanence and consistency were non-issues.  I plan to keep experimenting with Tachyons, next on my personal site and possible with a React Native project I have on the side.
+    </p>
+  </div>
+</article>
 )
