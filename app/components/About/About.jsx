a0b4cf3ac035a3912b296f85ccdc2a22e6532234
Student <jeff.kandel@gmail.com>
2017-07-28 08:11:25 -0400

    fix: edit about content

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -23,7 +23,7 @@ export default () => (
     Bike Shopper is not a store and was never intended to be one.  It is a design project with some meat on its bones.  After reading a lot about functional CSS, I was looking for a larger sandbox to explore functional CSS alongside React.  I decided to strip the CSS from an old  project and start from scratch with the Tachyons functional CSS library.  The site you see is a result of this experimentation.
     </p>
     <p className="lh-copy measure">
-    After working with Tachyons and React, I feel really positive about the combination.  Functional CSS feels close to the styling metal and the practice of writing out each class reinforced CSS fundamentals along the way.  Embedding these styles in React components ensured that maintanence and consistency were non-issues.  I plan to keep experimenting with Tachyons, next on my personal site and possible with a React Native project I have on the side.
+    After working with Tachyons and React, I feel really positive about the combination. Functional CSS feels close to the styling metal and the practice of writing out each class reinforced CSS fundamentals along the way. Embedding these styles in React components ensured that maintanence and consistency were non-issues. Tachyons is definitely a tool I will use again.
     </p>
   </div>
 </article>
